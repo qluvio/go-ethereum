2445c34100e83ac2cc2cbed98444755278989693
paulgoleary <paul@eluv.io>
2021-10-12 15:37:40 -0700

    WIP - port tests and elements of EIP 3436 clique fix.

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -27,6 +27,25 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
+// ChainHeaderReader defines a small collection of methods needed to access the local
+// blockchain during header verification.
+type ChainHeaderReader interface {
+	// Config retrieves the blockchain's chain configuration.
+	Config() *params.ChainConfig
+
+	// CurrentHeader retrieves the current header from the local chain.
+	CurrentHeader() *types.Header
+
+	// GetHeader retrieves a block header from the database by hash and number.
+	GetHeader(hash common.Hash, number uint64) *types.Header
+
+	// GetHeaderByNumber retrieves a block header from the database by number.
+	GetHeaderByNumber(number uint64) *types.Header
+
+	// GetHeaderByHash retrieves a block header from the database by its hash.
+	GetHeaderByHash(hash common.Hash) *types.Header
+}
+
 // ChainReader defines a small collection of methods needed to access the local
 // blockchain during header and/or uncle verification.
 type ChainReader interface {
